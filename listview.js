2286fb6936b5df2b2db55e73ae29a672572d8b02
Sofie <soba0003@stud.kea.dk>
2025-02-17 22:30:47 +0100

    listview look færdigt

--- listview.js
+++ listview.js
@@ -6,41 +6,43 @@ const recipelist = document.querySelector("#list_grid");
 let url = `https://dummyjson.com/recipes`;
 
 function getData() {
-	console.log("getData");
-	fetch(url)
-		.then((response) => response.json())
-		.then((data) => {
-			console.log("Modtaget data:", data); // Debugging
-			if (data.recipes && Array.isArray(data.recipes)) {
-				showRecipes(data.recipes);
-			} else {
-				console.error("Fejl: Ingen opskrifter fundet i data!", data);
-			}
-		})
-		.catch((error) => console.error("Fejl ved hentning af data:", error));
+  console.log("getData");
+  fetch(url)
+    .then((response) => response.json())
+    .then((data) => {
+      console.log("Modtaget data:", data); // Debugging
+      if (data.recipes && Array.isArray(data.recipes)) {
+        showRecipes(data.recipes);
+      } else {
+        console.error("Fejl: Ingen opskrifter fundet i data!", data);
+      }
+    })
+    .catch((error) => console.error("Fejl ved hentning af data:", error));
 }
 
 function showRecipes(recipes) {
-	console.log("showRecipes kaldt med:", recipes);
+  console.log("showRecipes kaldt med:", recipes);
 
-	const markup = recipes
-		.map(
-			(recipe) => `
-            <a class="list_card" href="#">
+  const markup = recipes
+    .map(
+      (recipe) => `
+	  <div>
+	  <a class="list_card" href="">
                 <img class="list_card_img" src="https://cdn.dummyjson.com/recipe-images/${recipe.id}.webp" alt="picture of dish" />
                 <h3 class="list_card_title">${recipe.name}</h3>
                 <div class="card_category_grid">
                     <h4 class="list_card_category">${recipe.cuisine}</h4>
                     <h4 class="list_card_category">${recipe.difficulty}</h4>
-                    <h4 class="list_card_category">${recipe.prepTimeMinutes} min</h4>
-                    <h4 class="list_card_category">${recipe.rating}⭐</h4>
+                    <h4 class="list_card_category">${recipe.prepTimeMinutes} MINUTES</h4>
+                    <h4 class="list_card_category">${recipe.rating} STARS</h4>
                 </div>
-            </a>
+				</a>
+			</div>
         `
-		)
-		.join("");
+    )
+    .join("");
 
-	recipelist.innerHTML = markup;
+  recipelist.innerHTML = markup;
 }
 
 // `<a class="list_card" href="">
