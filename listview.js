bd0332f51d5c113967ab3fc343f8da525de5fc3b
marc5950 <marcuswagner5950@gmail.com>
2025-02-17 11:49:57 +0100

    linker til single_view

--- listview.js
+++ listview.js
@@ -26,7 +26,7 @@ function showRecipes(recipes) {
 	const markup = recipes
 		.map(
 			(recipe) => `
-            <a class="list_card" href="#">
+            <a class="list_card" href="single_view.html?id=${recipe.id}">
                 <img class="list_card_img" src="https://cdn.dummyjson.com/recipe-images/${recipe.id}.webp" alt="picture of dish" />
                 <h3 class="list_card_title">${recipe.name}</h3>
                 <div class="card_category_grid">
