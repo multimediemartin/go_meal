5f2c2630b3557f459a1fe054226912b83d22e7fa
Sofie <soba0003@stud.kea.dk>
2025-02-17 22:41:28 +0100

    rigtige listview js (så den ikke bliver ændret i merge)

--- listview.js
+++ listview.js
@@ -6,42 +6,20 @@ const recipelist = document.querySelector("#list_grid");
 let url = `https://dummyjson.com/recipes`;
 
 function getData() {
-<<<<<<< HEAD
-	console.log("getData");
-	fetch(url)
-		.then((response) => response.json())
-		.then((data) => showRecipes(data.recipes));
-=======
   console.log("getData");
   fetch(url)
     .then((response) => response.json())
-    .then((data) => {
-      console.log("Modtaget data:", data); // Debugging
-      if (data.recipes && Array.isArray(data.recipes)) {
-        showRecipes(data.recipes);
-      } else {
-        console.error("Fejl: Ingen opskrifter fundet i data!", data);
-      }
-    })
-    .catch((error) => console.error("Fejl ved hentning af data:", error));
->>>>>>> listview-finish-ish
+    .then((data) => showRecipes(data.recipes));
 }
 
 function showRecipes(recipes) {
   console.log("showRecipes kaldt med:", recipes);
 
-<<<<<<< HEAD
-	const markup = recipes
-		.map(
-			(recipe) => `
-            <a class="list_card" href="single_view.html?id=${recipe.id}">
-=======
   const markup = recipes
     .map(
       (recipe) => `
-	  <div>
-	  <a class="list_card" href="">
->>>>>>> listview-finish-ish
+      <div>
+            <a class="list_card" href="single_view.html?id=${recipe.id}">
                 <img class="list_card_img" src="https://cdn.dummyjson.com/recipe-images/${recipe.id}.webp" alt="picture of dish" />
                 <h3 class="list_card_title">${recipe.name}</h3>
                 <div class="card_category_grid">
@@ -50,23 +28,11 @@ function showRecipes(recipes) {
                     <h4 class="list_card_category">${recipe.prepTimeMinutes} MINUTES</h4>
                     <h4 class="list_card_category">${recipe.rating} STARS</h4>
                 </div>
-				</a>
-			</div>
+            </a>
+            </div>
         `
     )
     .join("");
 
   recipelist.innerHTML = markup;
 }
-
-// `<a class="list_card" href="">
-//   <img class="list_card_img" src="https://cdn.dummyjson.com/recipe-images/${recipe.id}.webp" alt="picture of dish" />
-//   <h3 class="list_card_title">${recipe.name}</h3>
-//   <div class="card_category_grid">
-// <h4 class="list_card_category">${recipe.cuisine}</h4>
-// <h4 class="list_card_category">${recipe.difficulty}</h4>
-// <h4 class="list_card_category">${recipe.prepTimeMinutes}</h4>
-// <h4 class="list_card_category">${recipe.rating}</h4>
-//   </div>
-// </a>
-// `
